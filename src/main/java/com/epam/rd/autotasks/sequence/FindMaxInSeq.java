20d0adbfbb4e5711faab381bc1e35fd4fde94e93
Daniil Laureanti <daniil.laureanti@gmail.com>
2021-11-25 14:06:50 +0200

    Completed task

--- src/main/java/com/epam/rd/autotasks/sequence/FindMaxInSeq.java
+++ src/main/java/com/epam/rd/autotasks/sequence/FindMaxInSeq.java
@@ -1,15 +1,24 @@
 package com.epam.rd.autotasks.sequence;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class FindMaxInSeq {
     public static int max() {
 
-        // Put your code here
+        Scanner scanner = new Scanner(System.in);
 
-        return 0;
+        ArrayList<Integer> maxAverage = new ArrayList<>();
+
+        for (int i = 0, zero; (zero = scanner.nextInt()) !=0 ; i++) {
+            maxAverage.add(zero);
+        }
+
+        return Collections.max(maxAverage);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         System.out.println("Test your code here!\n");
 
